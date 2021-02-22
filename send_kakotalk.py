a4d82c24b3118d8a6f7a5819e41166b7c8034936
andyyeyeye <andyyeyeye@users.noreply.github.com>
2021-02-22 23:47:57 +0900

    ..

--- send_kakotalk.py
+++ send_kakotalk.py
@@ -1,10 +1,10 @@
 from kaling import Kakao
 
-KakaoLink = Kakao('87d3bd176110c4c614b5a877cbdf6eee','https://dfsdfsdfsdf.pythonanywhere.com')
+KakaoLink = Kakao('9f7088d9cbae02c3a17e64fbf7c69329','https://dfsdfsdfsdf.pythonanywhere.com')
 KakaoLink.login('10andyye.jongcye@gmail.com', 'kakao123@B')
 
 
-def send_kakaotalk(name,number):
+def send_kakaotalk_1(name,number):
     KakaoLink.send(name,{
                 "link_ver": "4.0",
                 "template_object": {
@@ -24,18 +24,91 @@ def send_kakaotalk(name,number):
                     "buttons": [{
                         "title": "37.5℃ 미만",
                         "link": {
-                            "web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&"+str(number)+"&0",
-                            "mobile_web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&"+str(number)+"&0"
+                            "web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&1&0",
+                            "mobile_web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&1&0"
                         }
                     },
                     {
-                        "title": "37.5℃ 이상 및 발열감",
+                        "title": "37.5℃ 이상",
                         "link": {
-                            "web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&"+str(number)+"&1",
-                            "mobile_web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&"+str(number)+"&1"
+                            "web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&1&1",
+                            "mobile_web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&1&1"
                         }
                     }]
                 }
             })
 
-send_kakaotalk("예상우",1)
+
+def send_kakaotalk_2(name,number):
+    KakaoLink.send(name,{
+                "link_ver": "4.0",
+                "template_object": {
+                    "object_type": "feed",
+                    "button_title": "",
+            
+                    "content": {
+                        "title": "감염이 의심되는 증상이 있나요?",
+                        "image_url": "https://dfsdfsdfsdf.pythonanywhere.com/static/infected.png",
+                        "link": {
+                            "web_url": "",
+                            "mobile_web_url": ""
+                        },
+                        "description": "made by 상우 in IDEV"
+                    },
+            
+                    "buttons": [{
+                        "title": "아니오",
+                        "link": {
+                            "web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&2&0",
+                            "mobile_web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&2&0"
+                        }
+                    },
+                    {
+                        "title": "예",
+                        "link": {
+                            "web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&2&1",
+                            "mobile_web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&2&1"
+                        }
+                    }]
+                }
+            })
+
+def send_kakaotalk_3(name,number):
+    KakaoLink.send(name,{
+                "link_ver": "4.0",
+                "template_object": {
+                    "object_type": "feed",
+                    "button_title": "",
+            
+                    "content": {
+                        "title": "본인 또는 동거인이 방역당국에 의해 현재 자가격리가 이루어지고 있나요?",
+                        "image_url": "https://dfsdfsdfsdf.pythonanywhere.com/static/isolation.png",
+                        "link": {
+                            "web_url": "",
+                            "mobile_web_url": ""
+                        },
+                        "description": "made by 상우 in IDEV"
+                    },
+            
+                    "buttons": [{
+                        "title": "아니오",
+                        "link": {
+                            "web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&3&0",
+                            "mobile_web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&3&0"
+                        }
+                    },
+                    {
+                        "title": "예",
+                        "link": {
+                            "web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&3&1",
+                            "mobile_web_url": "https://dfsdfsdfsdf.pythonanywhere.com/check/"+name+"&3&1"
+                        }
+                    }]
+                }
+            })
+
+
+
+send_kakaotalk_1("예상우",1)
+send_kakaotalk_2("예상우",1)
+send_kakaotalk_3("예상우",1)
