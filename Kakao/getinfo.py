1695caf1a0ff3773dd2dc24d3d0aaa7620dfe99e
andyyeyeye <andyyeyeye@users.noreply.github.com>
2021-02-27 16:05:00 +0900

    hmmm

--- Kakao/getinfo.py
+++ Kakao/getinfo.py
@@ -12,36 +12,36 @@ from datetime import datetime, time
 
 def is_time_between(begin_time, end_time, check_time=None):
     check_time = check_time or datetime.utcnow().time()
+    
     if begin_time < end_time:
         return check_time >= begin_time and check_time <= end_time
     else: 
         return check_time >= begin_time or check_time <= end_time
 
-        file = open("check.txt","w")
-        file.write("오전")
+def check_if_done_am():
+    file = open("Kakao\check.txt","r")
+    a = file.readline()
+    file.close()
+    if a == "am":
+        return False
+    else:
+        file = open("Kakao\check.txt","w")
+        file.write("am")
         file.close()
+        return True
 
-class MyClass(client.Client):
+def check_if_done_pm():
+    file = open("Kakao\check.txt","r")
+    a = file.readline()
+    file.close()
+    if a == "pm":
+        return False
+    else:
+        file = open("Kakao\check.txt","w")
+        file.write("pm")
+        file.close()
+        return True
 
-    async def onMessage(self, chat):
-        print(chat.message)
-        print(chat.rawBody)
-        print(chat.attachment)
-
-
-
-        if is_time_between(time(1,25), time(16,30)) and 
-
-        if chat.message == "/등록":
-            await chat.sendText("ksa.hs.kr 아이디와 비밀번호를\n/등록/아이디/비밀번호 형식으로 입력해 주세요")
-        
-        if chat.message[0:3] == "/등록":
-            if chat.message.count("/") == 3:
-                id = chat.message.split("/")[2].strip()
-                password = chat.message.split("/")[3].strip()
-                addperson(chat.authorId,id,password)
-                await chat.sendText("등록되었습니다! 작동이 되지 않는다면 다시 등록해주세요")
-        
 def addperson(chat_id, id, password):
 
     with open("Kakao\people.json", "r+") as file:
@@ -53,13 +53,42 @@ def addperson(chat_id, id, password):
         data[chat_id] = {"id":id,"password":password}
         file.seek(0)
         json.dump(data, file)
-            
-# addperson("0","1","2")
 
 def readperson(chat_id):
     with open('people.json') as f:
         data = json.load(f)
     return data
+
+
+
+
+class MyClass(client.Client):
+
+    print(1)
+
+    async def onMessage(self, chat):
+        print(chat.message)
+        print(chat.rawBody)
+        print(chat.attachment)
+
+
+
+        if chat.message == "/등록":
+            await chat.sendText("ksa.hs.kr 아이디와 비밀번호를\n/등록/아이디/비밀번호 형식으로 입력해 주세요")
+        
+        if chat.message[0:3] == "/등록":
+            if chat.message.count("/") == 3:
+                id = chat.message.split("/")[2].strip()
+                password = chat.message.split("/")[3].strip()
+                addperson(chat.authorId,id,password)
+                await chat.sendText("등록되었습니다! 작동이 되지 않는다면 다시 등록해주세요")
+        
+
+            
+# addperson("0","1","2")
+
+
+# print(check_if_done_am())
 
 client = MyClass("DEVICE")
 client.run("10andyye.jongcye@gmail.com", "kakao123@B")
